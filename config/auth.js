545927c071aea008adbd41b99b525d2222580288
meenalk44 <meenalskulkarni@gmail.com>
2017-05-17 18:06:21 -0700

    Changed Google Callback url

--- config/auth.js
+++ config/auth.js
@@ -3,6 +3,6 @@ module.exports = {
 		'googleAuth' : {
 	        'clientID'      : '572995275302-md7ogj5il8ani9qseooav6proqgb8r1b.apps.googleusercontent.com',
             'clientSecret'  : 'CZVW4o2y8HtPsWL2lqGiwSko',
-	        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+	        'callbackURL'   : 'https://edu-class.herokuapp.com/auth/google/callback'
 	    }
 };
