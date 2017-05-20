3cef0281d49ef4912aac42eb7bb5a75aae329222
meenalk44 <meenalskulkarni@gmail.com>
2017-05-19 20:29:23 -0700

    Added sample class to dbseed

--- helpers/dbSeed.js
+++ helpers/dbSeed.js
@@ -6,7 +6,6 @@ var User = require('../models/userSchema');
 User.find({email: "meenalskulkarni@gmail.com"}, function(err, users){
 	console.log(users);
 	if(users.length === 0) {
-		// ccreate user with email = meenal@asu.edu and role = teacher
 		var newUser = new User({
 			email: 'meenalskulkarni@gmail.com',
 			role: 'Teacher'
@@ -20,5 +19,48 @@ User.find({email: "meenalskulkarni@gmail.com"}, function(err, users){
 				
 			}
 		});
+        var newClass = new Class({
+            class_name	: 'Sample Class',
+            teacher_id : req.user.id,
+            template_A:{
+
+                template_discussion	:	'Nested',
+                template_rating	:	'UPVOTE'
+            },
+            template_B:    {
+                template_discussion	:	'Flat',
+                template_rating	:	'UPVOTE_DOWNVOTE'
+            }
+
+        });
+        newClass.save(function(err,entry){
+            if(err)
+                console.log(err);
+            else{
+                var newDF = new Discussion({
+                    class_id: entry.id
+
+                });
+                console.log("DF: "+ newDF);
+                newDF.save(function(err,entryDF){
+                    if(err)
+                        console.log("error in DF");
+                    else{
+                        console.log("*****&&& "+entryDF);
+                        Class.findByIdAndUpdate(entry.id, {$set:{'discussion_id' : entryDF.id}},{new:true}, function(err,docs){
+                            if(err)
+                                console.log(err)
+                            else{
+                                console.log(docs);
+
+                            }
+                        });
+
+
+                    }
+                });
+
+            }
+        });
 	}
 })
