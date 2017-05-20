13afeea95d8862211b0364267dfa3c16d581b46b
meenalk44 <meenalskulkarni@gmail.com>
2017-05-19 20:34:13 -0700

    Added sample class to dbseed

--- helpers/dbSeed.js
+++ helpers/dbSeed.js
@@ -16,51 +16,52 @@ User.find({email: "meenalskulkarni@gmail.com"}, function(err, users){
 				return console.log("Error while posting user to db in dbSeed.js");
 			}else{
 				console.log("User saved to db:  "+entry);
-				
-			}
-		});
-        var newClass = new Class({
-            class_name	: 'Sample Class',
-            teacher_id : req.user.id,
-            template_A:{
+                var newClass = new Class({
+                    class_name	: 'Sample Class',
+                    teacher_id : entry._id,
+                    template_A:{
 
-                template_discussion	:	'Nested',
-                template_rating	:	'UPVOTE'
-            },
-            template_B:    {
-                template_discussion	:	'Flat',
-                template_rating	:	'UPVOTE_DOWNVOTE'
-            }
-
-        });
-        newClass.save(function(err,entry){
-            if(err)
-                console.log(err);
-            else{
-                var newDF = new Discussion({
-                    class_id: entry.id
+                        template_discussion	:	'Nested',
+                        template_rating	:	'UPVOTE'
+                    },
+                    template_B:    {
+                        template_discussion	:	'Flat',
+                        template_rating	:	'UPVOTE_DOWNVOTE'
+                    }
 
                 });
-                console.log("DF: "+ newDF);
-                newDF.save(function(err,entryDF){
+                newClass.save(function(err,classEntry){
                     if(err)
-                        console.log("error in DF");
+                        console.log(err);
                     else{
-                        console.log("*****&&& "+entryDF);
-                        Class.findByIdAndUpdate(entry.id, {$set:{'discussion_id' : entryDF.id}},{new:true}, function(err,docs){
+                        var newDF = new Discussion({
+                            class_id: classEntry.id
+
+                        });
+                        console.log("DF: "+ newDF);
+                        newDF.save(function(err,entryDF){
                             if(err)
-                                console.log(err)
+                                console.log("error in DF");
                             else{
-                                console.log(docs);
+                                console.log("*****&&& "+entryDF);
+                                Class.findByIdAndUpdate(classEntry.id, {$set:{'discussion_id' : entryDF.id}},{new:true}, function(err,docs){
+                                    if(err)
+                                        console.log(err)
+                                    else{
+                                        console.log(docs);
+
+                                    }
+                                });
+
 
                             }
                         });
 
-
                     }
                 });
+				
+			}
+		});
 
-            }
-        });
 	}
-})
+});
